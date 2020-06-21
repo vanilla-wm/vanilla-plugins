576b86d0301ecac3cb57957b94f660cdbf211ecc
norbertdurcansky <norbert@cinnamon.video>
2020-06-21 15:15:34 +0200

    Fix version

--- packages/web-monetization/rollup.config.js
+++ packages/web-monetization/rollup.config.js
@@ -1,13 +1,12 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 import { terser } from 'rollup-plugin-terser'
 
 const bundleName = 'main'
 
-const build = (outFile, format, mode) => ({
+const build = (outFile, format) => ({
   input: 'src/index.ts',
   output: [
     {
@@ -20,20 +19,7 @@ const build = (outFile, format, mode) => ({
   watch: {
     include: 'src/**',
   },
-  plugins: [
-    json(),
-    typescript(),
-    commonjs(),
-    resolve(),
-    terser(),
-    sourceMaps(),
-  ],
+  plugins: [json(), typescript(), commonjs(), resolve(), terser()],
 })
 
-export default [
-  build(`${bundleName}.js`, 'cjs', 'development'),
-  build(`${bundleName}.min.js`, 'cjs', 'production'),
-  build(`${bundleName}.umd.js`, 'umd', 'development'),
-  build(`${bundleName}.umd.min.js`, 'umd', 'production'),
-  build(`${bundleName}.module.js`, 'es', 'development'),
-]
+export default [build(`${bundleName}.js`, 'cjs')]
